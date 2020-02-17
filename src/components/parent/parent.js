da15c85c38259091bb5b99dc62ea1d543b6a9b3b
Aspe1ta <ausy11@live.ca>
2020-02-17 17:23:05 -0500

    Update parent.js

--- src/components/parent/parent.js
+++ src/components/parent/parent.js
@@ -1,30 +1,33 @@
 import React from "react";
-import Landing from "./../components/landing/landing.js";
+import Landing from "./../landing/landing.js"
 import Search from "./../search/search.js"
 
 class Parent extends React.Component {
     
-
-    state = { query: "" }
+    
+    state = { query: "", mode: "land" }
 
   
 
-    callbackFunction = (childData) => {
-          this.setState({message: childData})
-          
-    }
+    callbackFunction = event => {
+      console.log(event.target.search.value);
+      event.preventDefault();
+      this.handleSave();
+      this.setState({ query: event.target.search.value, mode: "search" });
+    };
   
     render() {
 
-    
-
-
-    return (
+      if (this.state.mode === "land") {
+        return (
       <>
         <Landing call={this.callbackFunction}></Landing>
-        <Search></Search>
       </>
-    );
+    ) } else {
+      return(
+        <Search info={this.state.query}></Search>
+      )
+    } 
   }
 }
 
