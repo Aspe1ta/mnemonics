d81e3bdceba3381a62d9a541d757da505299c83a
Aspe1ta <ausy11@live.ca>
2020-03-26 15:15:25 -0400

    make it better

--- src/components/kin201/kin201.js
+++ src/components/kin201/kin201.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import Header from "./../header/header.js";
 import SideNav from "./../sideNav/sideNav.js";
 
-import groupImg from "./../../assets/AustinAssets-26.png";
+import groupImg from "./../../assets/AustinAssets-25.png";
 
 
 class Kin201 extends React.Component {
