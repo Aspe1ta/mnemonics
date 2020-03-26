84aab3f2c40d17332408673fe6bb18cdc9ea743e
Aspe1ta <ausy11@live.ca>
2020-03-26 14:25:31 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import axios from "axios";
 import Login from "./components/login/login.js";
 import Signup from "./components/signup/signup.js";
 import Verified from "./components/verified/verified.js";
-import StudyGroup from "./components/studyGroup/studyGroup.js";
+// import StudyGroup from "./components/studyGroup/studyGroup.js";
 import Kin201 from "./components/kin201/kin201.js";
 import Construction from "./components/construction/construction.js";
 import Saved from "./components/saved/saved.js";
@@ -94,7 +94,7 @@ class App extends React.Component {
           <Route path="/login" exact component={Login} />
           <Route path="/signUp" exact component={Signup} />
           <Route path="/verified" exact component={Verified} />
-          <Route path="/studyGroups" exact component={StudyGroup} />
+          {/* <Route path="/studyGroups" exact component={StudyGroup} /> */}
           <Route path="/kin201" exact component={Kin201} />
           <Route path="/saved" exact component={Saved} />
           <Route path="/notBuilt" exact component={Construction} />
